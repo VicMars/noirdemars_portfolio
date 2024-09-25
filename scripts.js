05f14befbeb1a3afd5882363357a2a341f13c95e
Vicky Andre <59230296+VicMars@users.noreply.github.com>
2024-09-25 19:07:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function animateH1() {
   console.log(h1);
   
   // Move the h1 to the right and set opacity to 1 over 2 seconds
-  h1.style.left = '300px';  // Move right by 300px
+  h1.style.left = '0';  // Move right by 300px
   h1.style.opacity = '1';
 
   // After 2.3 seconds (2s for movement + 0.3s at opacity 1), reset the opacity
