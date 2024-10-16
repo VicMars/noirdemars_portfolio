c95ddf4b97a657cfcd0c1d7160a5ce0fd94938e1
Vicky Andre <59230296+VicMars@users.noreply.github.com>
2024-10-16 11:17:52 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,14 @@ if (helloElements !== null) {
     // Add event listener for mouseover to change the target element's style
     element.addEventListener('mouseover', function() {
       // portraitImgWrapper.style.right = '20px'; // Change position on hover
-      portraitImgWrapper.style.transform = 'translateX(20px)';
+      portraitImg.style.transform = 'translateX(20px)';
       portraitImg.style.transform = 'rotate(0deg)'; // Change rotation on hover
     });
   
     // Add event listener for mouseout to reset the target element's style
     element.addEventListener('mouseout', function() {
       // portraitImgWrapper.style.right = '-80%'; // Reset position when no hover
-      portraitImgWrapper.style.transform = 'translateX(-80%)';
+      portraitImg.style.transform = 'translateX(-80%)';
       portraitImg.style.transform = 'rotate(5deg)'; // Reset rotation when no hover
     });
     
